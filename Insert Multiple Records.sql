197abe30b932358af49df84f2f31ff4782a3c322
Ruban <rubanpathak706@gmail.com>
2023-10-04 00:27:27 +0530

    New Added

--- Insert Multiple Records.sql
+++ Insert Multiple Records.sql
@@ -1,4 +1,9 @@
-INSERT INTO person(id, name, age, gender, phone)
+INSERT INTO person(id, name, age, gender, phone,city)
 VALUES
-(3,"Samarpita Mukherjee","24","F","7569823614"),
-(4,"Susmita Sinha","29","F","7521369458");
+(5,"Souvik Banerjee",20,"M","6541239874","Chuchura"),
+(6,"Jaya Bachhan",60,"F","7452136985","Mumbai"),
+(7,"Piu Swarnakar",23,"F","8964785214","Chennai"),
+(8,"Saikat Datta",25,"M","7852146932","Kolkata"),
+(9,"Sutam Paul",20,"M","7852176932","Noida"),
+(10,"Sk Md Altab",28,"M","7252146932","Chennai"),
+(11,"Shobhan Sen",22,"M","7892146932","Noida");

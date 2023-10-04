30a127d7ad50ce71461186589027e213ba8d3ff0
Ruban <rubanpathak706@gmail.com>
2023-10-04 18:48:23 +0530

    Like Operator And Wildcraft Character

---
+++ Like Operator.sql
@@ -0,0 +1,29 @@
+SELECT * FROM person 
+WHERE name LIKE "%a";
+
+SELECT * FROM person 
+WHERE name LIKE "a%";
+
+
+SELECT * FROM person 
+WHERE name LIKE "%am%";
+
+
+SELECT * FROM person 
+WHERE name LIKE "a%m";
+
+
+SELECT * FROM person 
+WHERE name LIKE "_a%";
+
+
+SELECT * FROM person 
+WHERE name LIKE "__a%";
+
+
+SELECT * FROM person 
+WHERE name LIKE "_ai";
+
+
+SELECT * FROM person 
+WHERE BINARY name LIKE "%e";
